2a34b832e81cd9b3daada360064cf47484eba51e
Sean Cunningham <sc.sean.cunningham@gmail.com>
2024-11-03 16:04:39 -0600

    cleans up code

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World! from Sean Cunningham 3308'
 
+
 @app.route('/db_test')
 def testing():
     conn = psycopg2.connect("postgresql://helloworlddb_fv6l_user:NkUYNSrQaEvMx9HYMV81udB0W1cpuADf@dpg-csjupddsvqrc73f0q91g-a/helloworlddb_fv6l")
     conn.close()
     return "Database Connection Successful"
+
 
 @app.route('/db_create')
 def db_create():
@@ -31,6 +33,7 @@ def db_create():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Created"
+
 
 @app.route('/db_insert')
 def db_insert():
